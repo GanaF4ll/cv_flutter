0e8fe0a3faf135bda45080ff58e76c85a416c798
GanaF4ll <ganafall9498@gmail.com>
2024-05-07 15:42:24 +0200

    createed competence.data

---
+++ lib/data/competence.data.dart
@@ -0,0 +1,22 @@
+List<Map> front = [
+  {"img": "html.svg", "name": "HTML"},
+  {"img": "css.svg", "name": "CSS"},
+  {"img": "javascript.svg", "name": "Javascript"},
+  {"img": "typescript.svg", "name": "Typescript"},
+  {"img": "angular.svg", "name": "Angular"},
+  {"img": "react.svg", "name": "React"},
+  {"img": "scss.svg", "name": "SCSS"},
+  {"img": "bootstrap2.svg", "name": "Bootstrap"},
+  {"img": "tailwind.svg", "name": "Tailwind"},
+  {"img": "nextjs.svg", "name": "Next.js"},
+];
+
+List<Map> back = [
+  {"img": "nodejs.svg", "name": "Node.js"},
+  {"img": "express.svg", "name": "Express"},
+  {"img": "mongodb.svg", "name": "MongoDB"},
+  {"img": "mysql.svg", "name": "MySQL"},
+  {"img": "prisma.svg", "name": "prisma"},
+  {"img": "mysql.svg", "name": "mysql"},
+  {"img": "sqlserver.svg", "name": "SQLSERVER"},
+];
